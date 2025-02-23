e0b377e939a546048778d5d732f9c48dcd25775f
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-02-23 23:39:59 +0900

    WordLookupTemplate

--- src/features/word/components/TopWordLookupTemplate.tsx
+++ src/features/word/components/TopWordLookupTemplate.tsx
@@ -4,27 +4,10 @@ import { useQuery } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { RingLoader } from "react-spinners";
+
+import { WordLookupTemplate } from "./WordLookupTemplate";
 
 import { getWordDefinitionApi } from "@/features/word/actions/word-api";
-
-import { RelatedWordList } from "@/features/word/components/RelatedWordList";
-
-import { SpeakButton, Button } from "@/shared/components/ui/button";
-import { Input } from "@/shared/components/ui/input";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/shared/components/ui/card";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from "@/shared/components/ui/form";
 
 import { sortPartOfSpeechArray } from "@/shared/lib/word";
 
@@ -66,134 +49,29 @@ export const TopWordLookupTemplate: FC<TopWordLookupTemplateProps> = ({
     return sortPartOfSpeechArray(resultList);
   }, [data?.data?.results]);
 
-  const onSubmit = useCallback(async (values: z.infer<typeof schema>) => {
-    const word = values.keyword;
+  const onSubmit = form.handleSubmit(
+    useCallback(async (values: z.infer<typeof schema>) => {
+      const word = values.keyword;
 
-    if (word) {
-      setInputKeyword(word);
-    }
-  }, []);
+      if (word) {
+        setInputKeyword(word);
+      }
+    }, [])
+  );
 
   if (error) {
     return <p>Error: {error.message}</p>;
   }
 
+  const resData = data?.data;
+
   return (
-    <div className="space-y-4">
-      <Card>
-        <CardHeader>
-          <CardTitle>Word Lookup</CardTitle>
-        </CardHeader>
-        <CardContent className="space-y-4">
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="flex gap-2">
-              <FormField
-                control={form.control}
-                name="keyword"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <Input placeholder="Enter a word..." {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button disabled={isLoading}>Search</Button>
-            </form>
-          </Form>
-        </CardContent>
-      </Card>
-
-      {isLoading && (
-        <div className="w-full h-48 flex items-center justify-center">
-          <RingLoader size={48} />
-        </div>
-      )}
-
-      {!isLoading && data?.data?.word && (
-        <div className="grid gap-4">
-          <Card className="w-full">
-            <CardHeader>
-              <CardTitle className="flex items-center justify-between">
-                <div className="gap-2 grid grid-flow-col auto-cols-max items-end">
-                  <h5>{data.data.word}</h5>
-                  {partOfSpeechList.map((partOfSpeech) => (
-                    <span
-                      key={partOfSpeech}
-                      className="text-muted-foreground text-sm"
-                    >
-                      {partOfSpeech}
-                    </span>
-                  ))}
-                </div>
-                <SpeakButton text={data.data.word} />
-              </CardTitle>
-            </CardHeader>
-
-            <CardContent>
-              {data?.data?.results &&
-                data?.data.results.map((result) => {
-                  return (
-                    <div
-                      key={result.partOfSpeech}
-                      className="space-y-4 border-t py-4"
-                    >
-                      <h2 className="font-medium">{result.partOfSpeech}</h2>
-                      <div className="grid gap-2">
-                        {result.definitions.map((result, i) => (
-                          <div
-                            key={`${result.definition}-${i}`}
-                            className="pl-4 grid gap-2"
-                          >
-                            <p className="font-medium">
-                              {i + 1}: {result.definition}
-                            </p>
-                            <div className="pl-8 grid gap-2">
-                              {result.examples && (
-                                <>
-                                  <h6 className="font-medium text-sm">
-                                    Example
-                                  </h6>
-                                  {result.examples.map((example, i) => (
-                                    <div
-                                      key={`${example}-${i}`}
-                                      className="flex gap-2"
-                                    >
-                                      <p
-                                        key={`${example}-${i}`}
-                                        className="text-muted-foreground flex items-center"
-                                      >
-                                        ・{example}
-                                      </p>
-                                      <SpeakButton text={example} />
-                                    </div>
-                                  ))}
-                                </>
-                              )}
-                              {result.synonyms && (
-                                <RelatedWordList
-                                  title="Synonyms"
-                                  wordList={result.synonyms}
-                                />
-                              )}
-                              {result.antonyms && (
-                                <RelatedWordList
-                                  title="Antonyms"
-                                  wordList={result.antonyms}
-                                />
-                              )}
-                            </div>
-                          </div>
-                        ))}
-                      </div>
-                    </div>
-                  );
-                })}
-            </CardContent>
-          </Card>
-        </div>
-      )}
-    </div>
+    <WordLookupTemplate
+      isLoading={isLoading}
+      data={resData}
+      form={form}
+      partOfSpeechList={partOfSpeechList}
+      onSubmit={onSubmit}
+    />
   );
 };
---
+++ src/features/word/components/WordLookupTemplate.tsx
@@ -0,0 +1,165 @@
+"use client";
+import { FC } from "react";
+import { type UseFormReturn } from "react-hook-form";
+import { RingLoader } from "react-spinners";
+
+import { RelatedWordList } from "@/features/word/components/RelatedWordList";
+
+import { SpeakButton, Button } from "@/shared/components/ui/button";
+import { Input } from "@/shared/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/shared/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/shared/components/ui/form";
+
+import { type ShowWordStateType } from "@/features/word/types/word";
+
+type WordLookupTemplateProps = {
+  isLoading: boolean;
+  data?: ShowWordStateType;
+  form: UseFormReturn<
+    {
+      keyword: string;
+    },
+    any, //eslint-disable-line @typescript-eslint/no-explicit-any
+    undefined
+  >;
+  partOfSpeechList: Array<string>;
+  onSubmit: (e?: React.BaseSyntheticEvent) => Promise<void>;
+};
+
+export const WordLookupTemplate: FC<WordLookupTemplateProps> = ({
+  isLoading,
+  data,
+  form,
+  partOfSpeechList,
+  onSubmit,
+}) => {
+  return (
+    <div className="space-y-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>Word Lookup</CardTitle>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <Form {...form}>
+            <form onSubmit={onSubmit} className="flex gap-2">
+              <FormField
+                control={form.control}
+                name="keyword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder="Enter a word..." {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button disabled={isLoading}>Search</Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+
+      {isLoading && (
+        <div className="w-full h-48 flex items-center justify-center">
+          <RingLoader size={48} />
+        </div>
+      )}
+
+      {!isLoading && data?.word && (
+        <div className="grid gap-4">
+          <Card className="w-full">
+            <CardHeader>
+              <CardTitle className="flex items-center justify-between">
+                <div className="gap-2 grid grid-flow-col auto-cols-max items-end">
+                  <h5>{data.word}</h5>
+                  {partOfSpeechList.map((partOfSpeech) => (
+                    <span
+                      key={partOfSpeech}
+                      className="text-muted-foreground text-sm"
+                    >
+                      {partOfSpeech}
+                    </span>
+                  ))}
+                </div>
+                <SpeakButton text={data.word} />
+              </CardTitle>
+            </CardHeader>
+
+            <CardContent>
+              {data?.results &&
+                data?.results.map((result) => {
+                  return (
+                    <div
+                      key={result.partOfSpeech}
+                      className="space-y-4 border-t py-4"
+                    >
+                      <h2 className="font-medium">{result.partOfSpeech}</h2>
+                      <div className="grid gap-2">
+                        {result.definitions.map((result, i) => (
+                          <div
+                            key={`${result.definition}-${i}`}
+                            className="pl-4 grid gap-2"
+                          >
+                            <p className="font-medium">
+                              {i + 1}: {result.definition}
+                            </p>
+                            <div className="pl-8 grid gap-2">
+                              {result.examples && (
+                                <>
+                                  <h6 className="font-medium text-sm">
+                                    Example
+                                  </h6>
+                                  {result.examples.map((example, i) => (
+                                    <div
+                                      key={`${example}-${i}`}
+                                      className="flex gap-2"
+                                    >
+                                      <p
+                                        key={`${example}-${i}`}
+                                        className="text-muted-foreground flex items-center"
+                                      >
+                                        ・{example}
+                                      </p>
+                                      <SpeakButton text={example} />
+                                    </div>
+                                  ))}
+                                </>
+                              )}
+                              {result.synonyms && (
+                                <RelatedWordList
+                                  title="Synonyms"
+                                  wordList={result.synonyms}
+                                />
+                              )}
+                              {result.antonyms && (
+                                <RelatedWordList
+                                  title="Antonyms"
+                                  wordList={result.antonyms}
+                                />
+                              )}
+                            </div>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  );
+                })}
+            </CardContent>
+          </Card>
+        </div>
+      )}
+    </div>
+  );
+};
